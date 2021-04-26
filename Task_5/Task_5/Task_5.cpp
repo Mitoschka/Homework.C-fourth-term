f9e9e8915e6135ea911740c6fe4b69f56945901e
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2021-04-27 03:23:44 +0600

    Add Task_5

---
+++ Task_5/Task_5/Task_5.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int NumberOfGuests = 0;
+	cout << "Please, enter the total number of guests: ";
+	cin >> NumberOfGuests;
+	vector<int> arrivals;
+	vector<int> exits;
+	int tempToInput = 0;
+	// Inputing array of arrivals
+	for (int i = 0; i < NumberOfGuests; i++)
+	{
+		cout << "Please, enter the time when " << i + 1 << " guest arrived: ";
+		cin >> tempToInput;
+		arrivals.push_back(tempToInput);
+	}
+	int maximumTimeOfExits = 0;
+	// Inputing array of exits
+	for (int i = 0; i < NumberOfGuests; i++)
+	{
+		cout << "Please, enter the time when " << i + 1 << " guest left: ";
+		cin >> tempToInput;
+		if (maximumTimeOfExits < tempToInput)
+		{
+			maximumTimeOfExits = tempToInput;
+		}
+		exits.push_back(tempToInput);
+	}
+	int maxAmountOfGuests = 0;
+	int tempAmountOfGuests = 0;
+	int timeToMaxGuests = 0;
+	for (int t = 1; t < maximumTimeOfExits; t++)
+	{
+		tempAmountOfGuests = 0;
+		for (int i = 0; i < NumberOfGuests; i++)
+		{
+			if (arrivals[i] <= t && exits[i] >= t)
+			{
+				tempAmountOfGuests++;
+			}
+		}
+		if (tempAmountOfGuests > maxAmountOfGuests)
+		{
+			maxAmountOfGuests = tempAmountOfGuests;
+			timeToMaxGuests = t;
+		}
+	}
+	cout << "The time when the party is attended by the maximum number of guests: " << timeToMaxGuests;
+	return 0;
+}
